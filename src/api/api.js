627e2eeaef6d9c3ebf001d9b6295f490763c8037
baozoudashijian <Yhldcahcy521>
2022-01-11 17:54:14 +0800

    前期准备工作: 配置数据请求

---
+++ src/api/api.js
@@ -0,0 +1,22 @@
+import Server from "./server";
+
+class API extends Server {
+  async uploadImg(params = {}) {
+    try {
+      let result = await this.axios('post', '//elm.cangdu.org/v1/addimg/shop', params)
+      if (result && result.status === 1) {
+        return result
+      } else {
+        let err = {
+          tip: '上传图片失败',
+          response: result,
+          data: params,
+          url: '//elm.cangdu.org/v1/addimg/shop'
+        }
+        throw err;
+      }
+    } catch (err) {
+      throw err
+    }
+  }
+}
