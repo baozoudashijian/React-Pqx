d92425081a6ca580c0040d5c4d811338bc2b9443
baozoudashijian <Yhldcahcy521>
2022-01-17 18:24:35 +0800

    完成帮助中心页面

--- src/pages/helpcenter/helpcenter.jsx
+++ src/pages/helpcenter/helpcenter.jsx
@@ -1,12 +1,29 @@
-import React, { Component } from'react';
+import React, {Component} from 'react';
 import PublicHeader from '@/components/header/header'
+import './helpcenter.scss'
 
 class Helpcenter extends Component {
 
   render() {
     return (
-      <main className="helpcenter-container">
+      <main>
         <PublicHeader title="帮助中心" record></PublicHeader>
+        <article className="context-con">
+          <h2>介绍</h2>
+          <p>本项目主要用于理解 react 和 redux 的编译方式，以及 react + redux 之间的配合方式</p>
+          <h2>技术要点</h2>
+          <p>react：v17.0.2</p>
+          <p>redux：v4.1.2</p>
+          <p>webpack：v5.64.4</p>
+          <p>react-router：v4.2.2</p>
+          <p>axios：v0.24.0</p>
+          <p>sass：v7.0.1</p>
+          <p>immutable：v3.8</p>
+          <p>ES 6/7/8</p>
+          <p>code split</p>
+          <p>hot loader</p>
+          <p>项目地址 <a href="https://github.com/baozoudashijian/React-Pqx">github</a></p>
+        </article>
       </main>
     )
   }
